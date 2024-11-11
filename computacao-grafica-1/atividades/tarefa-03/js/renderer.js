1ec939e118f5d4aeb6750bf0766026b990b73eba
Leonardo Costa <lbcosta.dev@gmail.com>
2024-11-11 20:27:01 -0300

    cg1-tarefa03: adiciona parâmetros da tarefa 3

---
+++ computacao-grafica-1/atividades/tarefa-03/js/renderer.js
@@ -0,0 +1,142 @@
+class Renderer {
+    #gl
+    #program
+    #numVertices
+    #canvasWidth
+    #canvasHeight
+
+    constructor() {
+        const canvas = document.getElementById("canvas")
+        if (!canvas) {
+            console.error("Canvas não encontrado.")
+            return
+        }
+
+        this.#canvasWidth = canvas.width
+        this.#canvasHeight = canvas.height
+        
+        this.#gl = canvas.getContext('webgl')
+        if (!this.#gl) {
+            throw new Error("Seu navegador não suporta WebGL.")
+        }
+
+        this.programReady = this.#init()
+
+        return new Proxy(this, {
+            get: (target, prop) => {
+                const value = target[prop]
+                if (typeof value === 'function' && prop !== '#init' && prop !== '#createShader' && prop !== '#createProgram') {
+                    return async (...args) => {
+                        await target.programReady
+                        return value.apply(target, args)
+                    }
+                }
+                return value
+            }
+        })
+    }
+
+    async #init() {
+        const vertexShader = await this.#createShader(this.#gl.VERTEX_SHADER, "shaders/vertex.glsl")
+        if (!vertexShader) {
+            throw new Error("Erro ao criar o Vertex Shader.")
+        }
+
+        const fragmentShader = await this.#createShader(this.#gl.FRAGMENT_SHADER, "shaders/fragment.glsl")
+        if (!fragmentShader) {
+            throw new Error("Erro ao criar o Fragment Shader.")
+        }
+
+        this.#program = this.#createProgram(this.#gl, vertexShader, fragmentShader)
+        if (!this.#program) {
+            throw new Error("Erro ao criar o Programa.")
+        }
+
+        this.#gl.useProgram(this.#program)
+    }
+
+    async #createShader(type, shaderPath) {
+        let source
+        try {
+            const response = await fetch(shaderPath)
+            if (!response.ok) {
+                throw new Error(`Failed to load shader: ${response.statusText}`)
+            }
+            source = await response.text()
+        } catch (error) {
+            console.error(error)
+            return null
+        }
+
+        const shader = this.#gl.createShader(type)
+        this.#gl.shaderSource(shader, source)
+        this.#gl.compileShader(shader)
+        if (!this.#gl.getShaderParameter(shader, this.#gl.COMPILE_STATUS)) {
+            console.error(this.#gl.getShaderInfoLog(shader))
+            this.#gl.deleteShader(shader)
+            return null
+        }
+        
+        return shader
+    }
+
+    #createProgram(gl, vertexShader, fragmentShader) {
+        const program = gl.createProgram()
+        gl.attachShader(program, vertexShader)
+        gl.attachShader(program, fragmentShader)
+        gl.linkProgram(program)
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            console.error(gl.getProgramInfoLog(program))
+            gl.deleteProgram(program)
+            return null
+        }
+
+        return program
+    }
+
+    setPositionBuffer(positions) {
+        const positionAttributeLocation = this.#gl.getAttribLocation(this.#program, "a_position")
+        const positionBuffer = this.#gl.createBuffer()
+        this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, positionBuffer)
+        this.#gl.bufferData(this.#gl.ARRAY_BUFFER, new Float32Array(positions), this.#gl.STATIC_DRAW)
+        this.#gl.enableVertexAttribArray(positionAttributeLocation)
+        this.#gl.vertexAttribPointer(positionAttributeLocation, 2, this.#gl.FLOAT, false, 0, 0)
+        
+        this.#numVertices = positions.length
+    }
+
+    setParams(params) {
+        if (typeof params !== 'object') {
+            throw new Error("Params deve ser um objeto.")
+        }
+
+        for (let [name, value] of Object.entries(params)) {
+            const location = this.#gl.getUniformLocation(this.#program, name)
+
+            if (typeof value === 'number') {
+                this.#gl.uniform1f(location, value)
+            } else if (Array.isArray(value)) {
+                switch (value.length) {
+                    case 2: this.#gl.uniform2fv(location, value); break
+                    case 3: this.#gl.uniform3fv(location, value); break
+                    case 4: this.#gl.uniform4fv(location, value); break
+                    default: throw new Error("Dimensões inválidas.")
+                }
+            } else {
+                throw new Error("Valor inválido.")
+            }
+        }
+
+        this.#gl.uniform1f(this.#gl.getUniformLocation(this.#program, "canvasWidth"), this.#canvasWidth)
+        this.#gl.uniform1f(this.#gl.getUniformLocation(this.#program, "canvasHeight"), this.#canvasHeight)
+    }
+
+    clear(color) {
+        this.#gl.clearColor(color.r, color.g, color.b, 1)
+        this.#gl.clear(this.#gl.COLOR_BUFFER_BIT)
+    }
+
+    render() {
+        this.#gl.drawArrays(this.#gl.TRIANGLES, 0, this.#numVertices)
+    }
+}
---
+++ computacao-grafica-1/atividades/tarefa-03/shaders/fragment.glsl
@@ -0,0 +1,106 @@
+precision mediump float;
+
+// tela
+uniform float canvasWidth; // pixels
+uniform float canvasHeight; // pixels
+uniform float nCol; // número de colunas
+uniform float nLin; // número de linhas
+
+// camera
+uniform float wJanela; // metros
+uniform float hJanela; // metros
+uniform float dJanela; // metros
+
+// esfera
+uniform float rEsfera; // metros
+uniform vec3 centroEsfera;
+
+// cores
+uniform vec3 bgColor;
+
+// iluminação
+uniform vec3 lightSourceIntensity;
+uniform vec3 lightSourcePosition;
+uniform vec3 ambientLightIntensity;
+uniform vec3 sphereAmbientReflection;
+uniform vec3 sphereDiffuseReflection;
+uniform vec3 sphereSpecularReflection;
+uniform float sphereShininess;
+
+// // chão
+uniform vec3 floorPosition;
+uniform vec3 floorNormal;
+uniform vec3 floorAmbientReflection;
+uniform vec3 floorDiffuseReflection;
+uniform vec3 floorSpecularReflection;
+uniform float floorShininess;
+
+// plano de fundo
+uniform vec3 backgroundPosition;
+uniform vec3 backgroundNormal;
+uniform vec3 backgroundAmbientReflection;
+uniform vec3 backgroundDiffuseReflection;
+uniform vec3 backgroundSpecularReflection;
+uniform float backgroundShininess;
+
+void main() {
+    // camera
+    vec3 origin = vec3(0.0, 0.0, 0.0);
+
+    // X do raio emitido
+    float deltaX = wJanela / nCol;
+    float col = floor(gl_FragCoord.x * (nCol / canvasWidth));
+    float x = (-wJanela / 2.0) + (deltaX / 2.0) + col * deltaX;
+
+    // Y do raio emitido
+    float deltaY = hJanela / nLin;
+    float lin = floor(gl_FragCoord.y * (nLin / canvasHeight));
+    float y = (hJanela / 2.0) - (deltaY / 2.0) - lin * deltaY;
+
+    // Z do raio emitido
+    float z = -dJanela;
+
+    // Raio emitido
+    vec3 ray = vec3(x, y, z);
+    ray = normalize(ray);
+
+    // Raio retornado
+    vec3 reflectedRay = origin - centroEsfera;
+
+    // Coeficientes da equação do 2º grau
+    float a = dot(ray, ray);
+    float b = 2.0 * dot(reflectedRay, ray);
+    float c = dot(reflectedRay, reflectedRay) - rEsfera * rEsfera;
+
+    // Delta da equação do 2º grau
+    float delta = b * b - 4.0 * a * c;
+
+    // Se delta >= 0, então existem pontos que satisfazem a equação do 2º grau, ou seja, o raio intercepta a esfera.
+    if (delta >= 0.0) {
+        // Interseção
+        float distance = (-b - sqrt(delta)) / (2.0 * a);
+        vec3 intersectionPoint = origin + distance * ray; // p(t) = p0 + t * dr
+        vec3 n = normalize(intersectionPoint - centroEsfera);
+        
+        // Iluminação ambiente da esfera
+        vec3 ambientLighting = sphereAmbientReflection * ambientLightIntensity; // Iamb = Ka * Ia
+
+        // Iluminação difusa da esfera
+        vec3 l = normalize(lightSourcePosition - intersectionPoint); // l é o vetor unitário que aponta para a fonte de luz
+        float diffuseAttenuationFactor = max(dot(l, n), 0.0); // Fator de atenuação da luz. Se for 0, a luz não incide na superfície.
+        vec3 diffuseLighting = sphereDiffuseReflection * lightSourceIntensity * diffuseAttenuationFactor; // Idif = Kd * I * (l . n)
+
+        // Iluminação especular da esfera
+        vec3 r = 2.0 * (dot(l, n)) * n - l; // r é o vetor de reflexão da luz
+        vec3 v = normalize(-ray); // v é o vetor unitário que aponta para a câmera
+        float specularAttenuationFactor = pow(max(dot(r, v), 0.0), sphereShininess); // Fator de atenuação da luz especular = (r . v)^α
+        vec3 specularLighting = sphereSpecularReflection * lightSourceIntensity * specularAttenuationFactor; // Ispec = Ks * I * (r . v)^α
+
+        // Cor final
+        vec3 seenColor = ambientLighting + diffuseLighting + specularLighting; // Ieye = Iamb + Idif + Ispec
+        gl_FragColor = vec4(seenColor, 1.0);
+    } else {
+        // Sem interseção
+        gl_FragColor = vec4(bgColor, 1.0);
+    }
+}
